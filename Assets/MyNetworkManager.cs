fbb02a6dd0fe40a1e7fc4c35069b37831dd8a828
b <biro123@ntlworld.com>
2017-12-02 16:57:04 +0000

    Replaced MyHUD with MyNetworkManager to inherit from NetworkManager

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+    public void MyStartHost()
+    {
+        Debug.Log("Starting Host " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Started Host " + Time.timeSinceLevelLoad);
+    }
+}
