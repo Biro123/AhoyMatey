fcae36bf493ad2993daa64f35c42e0d0730e31b4
b <biro123@ntlworld.com>
2017-12-02 21:53:31 +0000

    corrections for Local Player Authority and multiple Audio Listeners

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -24,6 +24,7 @@ public class Player : NetworkBehaviour {
         if (isLocalPlayer)
         {
             GetComponentInChildren<Camera>().enabled = true;
+            GetComponentInChildren<AudioListener>().enabled = true;
         }
     }
 }
